4cfd7a497e7fbf2875189564ad0a41d90150f289
Jayanthi Ramalingam <jayanthir@stg.com>
2022-03-29 17:15:04 +0530

    helpdesk angular

---
+++ src/app/employee.ts
@@ -0,0 +1,13 @@
+import { DepFlags } from "@angular/compiler/src/core";
+
+export class Employee {
+  id: number;
+  empname: string;
+  empid: string;
+  emailid: string;
+  itteam: string;
+  description: string;
+  flag:string;
+  enddate:Date;
+  active: boolean;
+}
---
+++ src/app/employee.ts.ts
@@ -0,0 +1,11 @@
+export class Employee {
+    id: number;
+  empname: string;
+  empid: string;
+  emailId: string;
+  itteam: string;
+  describtion: string;
+  flag:string;
+  enddate:Date;
+  active: boolean;
+}
